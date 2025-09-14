fa2582cfbe6cef38a1254d1f9b0a824d5c13098b
Ssemaganda-George <ssemagandageorge480@gmail.com>
2025-09-14 09:26:14 +0300

    New changes

--- src/components/home/ImpactSummary.tsx
+++ src/components/home/ImpactSummary.tsx
@@ -8,7 +8,7 @@ const ImpactSummary = () => {
           <div className="flex items-center">
             <Leaf className="h-5 w-5 mr-2 text-safari-green" />
             <span className="text-sm">
-              <strong className="font-bold text-safari-green">765</strong> trees planted
+              <strong className="font-bold text-safari-green">1000</strong> trees planted
             </span>
           </div>
           
