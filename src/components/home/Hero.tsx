4a0a285676ab1cc9a3c2f896fb0baf31a43d60e4
Ssemaganda-George <ssemagandageorge480@gmail.com>
2026-06-02 09:18:30 +0300

    Updated the carbon amount

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -81,7 +81,7 @@ const Hero = () => {
                   description: 'Operators, suppliers and tourism brands onboarded.',
                 },
                 {
-                  value: '60+ KG',
+                  value: '100+ KG',
                   label: 'Carbon Offset',
                   description: 'Impact linked to sustainable travel initiatives like tree planting.',
                 },
